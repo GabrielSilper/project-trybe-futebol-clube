7a9bfdf29a01b19cf3d0aa77882382ff37842286
GabrielSilper <gabrielsilper.main@gmail.com>
2023-06-13 20:54:02 -0400

    R04 - TDD do endpoint GET /teams/:id criado.

--- app/backend/src/tests/Team.test.ts
+++ app/backend/src/tests/Team.test.ts
@@ -1,27 +1,47 @@
 import * as sinon from 'sinon';
 import * as chai from 'chai';
 import { app } from '../app';
-import Example from '../database/models/ExampleModel';
-import { Response } from 'superagent';
-import SequelizeTeam from '../database/models/SequelizeTeam'
-import { OK } from '../constants/httpCodes';
+import SequelizeTeam from '../database/models/SequelizeTeam';
+import { NOT_FOUND, OK } from '../constants/httpCodes';
 
 // @ts-ignore
 import chaiHttp = require('chai-http');
-import { teams } from './mocks/Team.mocks';
+import { teams, team1 } from './mocks/Team.mocks';
 chai.use(chaiHttp);
 
 const { expect } = chai;
 
 describe('Team test', () => {
+  afterEach(() => {
+    sinon.restore();
+  })
   describe('GET part from /teams', () => {
-    it('return all books', async () => {
-      sinon.stub(SequelizeTeam, 'findAll').resolves(teams as any)
+    it('returns all books', async () => {
+      sinon.stub(SequelizeTeam, 'findAll').resolves(teams as any);
 
       const { status, body } = await chai.request(app).get('/teams');
 
       expect(status).to.be.equal(OK);
       expect(body).to.be.deep.equal(teams);
+    });
+
+    it('returns an error message: "Book with id (?) not found."', async () => {
+      sinon.stub(SequelizeTeam, 'findByPk').resolves(null);
+
+      const { status, body } = await chai.request(app).get('/teams/:1000');
+
+      expect(status).to.be.equal(NOT_FOUND);
+      expect(body).to.be.deep.equal({ message: 'Book with id 1000 not found.'});
+    });
+
+    it('returns a book correctly', async () => {
+      const theTeam = SequelizeTeam.build(team1)
+      sinon.stub(SequelizeTeam, 'findByPk').resolves(theTeam);
+
+      const { status, body } = await chai.request(app).get('/teams/:1');
+
+      expect(status).to.be.equal(OK);
+      expect(body).to.be.deep.equal(team1);
     });
   });
 });
